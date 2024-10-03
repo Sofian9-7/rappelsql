79a8f38bd2ae3816717ea4307ae58e24496883bd
Sofian Dimassi <sofian.dimassi97@gmail.com>
2024-10-03 14:49:07 +0200

    exercice 7

--- rappelsql.sql
+++ rappelsql.sql
@@ -7,3 +7,13 @@ SELECT nom, email FROM clients;
 SELECT nom, email FROM clients WHERE age > 40;
 
 
+SELECT * FROM clients WHERE location = 'Paris' AND age BETWEEN 25 AND 35
+
+
+SELECT * FROM campagnes ORDER BY budget ASC;
+
+
+SELECT * FROM campagnes ORDER BY debut_campagne DESC;
+
+
+SELECT COUNT(*) AS total_clients FROM clients
